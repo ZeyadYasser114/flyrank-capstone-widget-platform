8e3ee6c3ecbd3ce43aa78462db8a5ae0e6f3aeba
Zeyad Yasser <zeyad2007.4y@gmail.com>
2026-08-30 20:15:38 +0300

    Phase 2: add geo enrichment with provider fallback chain

---
+++ src/services/enrichment.service.js
@@ -0,0 +1,23 @@
+ async function enrichIp(ip) {
+    try {
+        const responseA = await fetch(`http://ip-api.com/json/${ip}`);
+        const data = await responseA.json();
+        if (data.status === 'success') {
+            return { country: data.country, city: data.city };
+        }
+    } catch (err) {
+
+    }
+
+    try{
+        const responseB = await fetch(`https://ipapi.co/${ip}/json/`);
+        const data = await responseB.json();
+        if (!data.error){
+            return {country: data.country_name , city: data.city};
+        }
+    } catch (err){
+        return { country: null, city: null };
+    }
+}
+
+module.exports = { enrichIp };
